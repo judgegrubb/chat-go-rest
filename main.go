14bb79d5f9098af5750a6942fea78242f379e260
judgegrubb <elijah.grubb@utah.edu>
2015-12-29 00:28:03 -0700

    implement basic structure and add gitignore

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
 	"net/http"
-    
-    "github.com/gorilla/mux"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+type Message struct {
+	Id      int       `json:"id"`
+	Message string    `json:"message"`
+	Author  string    `json:"author"`
+	Created time.Time `json:"created"`
+}
+
+type Messages []Message
+
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
+	router.HandleFunc("/messages", messages)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hellow, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hellow, %q", html.EscapeString(r.URL.Path))
+}
+
+func messages(w http.ResponseWriter, r *http.Request) {
+	messages := Messages{
+		Message{Id: 1, Message: "Hello!", Author: "fred", Created: time.Now()},
+		Message{Id: 2, Message: "What's up?", Author: "tEd", Created: time.Now()},
+	}
+
+	json.NewEncoder(w).Encode(messages)
 }
